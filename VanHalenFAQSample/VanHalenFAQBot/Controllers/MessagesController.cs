535e19b6238633ef7a56609c821fbebe59b7e19a
髙尾 <tetsuro.takao@processtune.com>
2018-04-28 15:33:11 +0900

    CosmosDBのベース取り込み

---
+++ VanHalenFAQSample/VanHalenFAQBot/Controllers/MessagesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace VanHalenFAQBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            #region
+            //var userAccount = new ChannelAccount(name: "Larry", id: "b106dbec-356a-4ab0-87bb-7a8656ef12eb");
+            //var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            //var length = activity.Text.Length;
+            //Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+            //await connector.Conversations.ReplyToActivityAsync(reply);
+
+            //var connector = new ConnectorClient(new Uri(actvity.ServiceUrl));
+            //var conversationId = await connector.Conversations.CreateDirectConversationAsync(botAccount, userAccount);
+
+            //IMessageActivity message = Activity.CreateMessageActivity();
+            //message.From = botAccount;
+            //message.Recipient = userAccount;
+            //message.Conversation = new ConversationAccount(id: conversationId.Id);
+            //message.Text = "Hello, Larry!";
+            //message.Locale = "en-Us";
+            //await connector.Conversations.SendToConversationAsync((Activity)message);
+            #endregion
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                new Task(async () =>
+                {
+                    await Conversation.SendAsync(message, () => new Dialogs.RootDialog());
+                }).RunSynchronously();
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+                System.Diagnostics.Debug.Print("");
+            }
+
+            return null;
+        }
+    }
+}
---
+++ VanHalenFAQSample/VanHalenFAQBot/Dialogs/RootDialog.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace VanHalenFAQBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            string message = string.Empty;
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+            string message = string.Empty;
+            if (activity.Type == "conversationUpdate")
+            {
+                message = "こんにちは";
+            }
+            else
+            {
+                int length = (activity.Text ?? string.Empty).Length;
+                message = $"You sent {activity.Text} which was {length} characters";
+                //CosmosDBで質問を補正
+                //QnA Makerへ問合せ
+            }
+            if (context.UserData.Count==0)
+            {
+                await context.PostAsync(message);
+            }
+            context.UserData.SetValue("greeting", "yes");
+            activity.Value = message;
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+    class MessageActivity : IMessageActivity
+    {
+        public string Locale { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Text { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Speak { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string InputHint { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Summary { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string TextFormat { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string AttachmentLayout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IList<Attachment> Attachments { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public SuggestedActions SuggestedActions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IList<Entity> Entities { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Importance { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DeliveryMode { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset? Expiration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public object Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ServiceUrl { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset? Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTimeOffset? LocalTimestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ChannelId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ChannelAccount From { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ConversationAccount Conversation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ChannelAccount Recipient { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ReplyToId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public dynamic ChannelData { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public IContactRelationUpdateActivity AsContactRelationUpdateActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IConversationUpdateActivity AsConversationUpdateActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEndOfConversationActivity AsEndOfConversationActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEventActivity AsEventActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IInstallationUpdateActivity AsInstallationUpdateActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IInvokeActivity AsInvokeActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageActivity AsMessageActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageDeleteActivity AsMessageDeleteActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageReactionActivity AsMessageReactionActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessageUpdateActivity AsMessageUpdateActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISuggestionActivity AsSuggestionActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITraceActivity AsTraceActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITypingActivity AsTypingActivity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeT GetChannelData<TypeT>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Mention[] GetMentions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetChannelData<TypeT>(out TypeT instance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
